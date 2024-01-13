5de96a000f9efdfe204a1d9b1ac53913a89712c8
Andrei Scafi <andreiscafi@hotmail.com>
2024-01-13 08:42:30 -0300

    Adding hover effect with Javascript.

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 const leftArrow = document.querySelector('.left-arrow');
-const rightArrow = document.querySelector('.right-arrow')
+const rightArrow = document.querySelector('.right-arrow');
 const cubes = document.querySelectorAll('.cube');
 
 let x = 0;
 
+const rotate = () => {
+    cubes.forEach(cube => cube.style.transform = `rotateY(${x}deg)`);
+}
+
 
 leftArrow.addEventListener('click', () => {
     x += 90;
-    cubes.forEach(cube => cube.style.transform = `rotateY(${x}deg)`);
+    rotate();
 });
 
 rightArrow.addEventListener('click', () => {
     x -= 90;
-    cubes.forEach(cube => cube.style.transform = `rotateY(${x}deg)`);
+    rotate();
+});
+
+//Hover Effect
+
+leftArrow.addEventListener('mouseover', () => {
+    x += 25;
+    rotate();
+});
+
+leftArrow.addEventListener('mouseout', () => {
+    x -= 25;
+    rotate();
+});
+
+rightArrow.addEventListener('mouseover', () => {
+    x -= 25;
+    rotate();
+});
+
+rightArrow.addEventListener('mouseout', () => {
+    x += 25;
+    rotate();
 });
